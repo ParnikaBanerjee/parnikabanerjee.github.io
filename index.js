f237054534b522d324f62cc9efc22895273039d8
unknown <parnikab127@gmail.com>
2024-10-04 01:06:49 +0530

    next iteration

---
+++ index.js
@@ -0,0 +1,22 @@
+/*import { GSAPInfoBar } from "https://codepen.io/GreenSock/pen/vYqpyLg.js"
+new GSAPInfoBar({ link: "https://gsap.com/docs/v3/Plugins/ScrollTrigger/"});
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".hi",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+      markers: true,
+      id: "scrub"
+    } 
+  })
+  tl.to(".hi", {
+    duration: 2,
+    x: 1000,
+    rotation: 360,
+  })
+  .to(".hi", {
+    duration: 2
+  });*/
+  
